d83e4c583f96d27076ad6a7ae863b5fdef3c7011
Syun199986 <syun990806@gmail.com>
2022-10-11 12:03:20 +0900

    04-1 done to Practice04-2

---
+++ 04-1/Practice04-2.php
@@ -0,0 +1,12 @@
+<?php
+$str = "test";
+$int = 1;
+$arr = [1, 2, 3];
+$bool = true;
+$N = NULL;
+
+echo gettype($str) . PHP_EOL;
+echo gettype($int) . PHP_EOL;
+echo gettype($arr) . PHP_EOL;
+echo gettype($bool) . PHP_EOL;
+echo gettype($N) . PHP_EOL;
